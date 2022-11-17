11e96407c6189dff77dfc4bd23367b28703c4435
GurovaAnya <anna30850@gmail.com>
2022-11-17 22:29:42 +0800

    added is_redeemed for easier calculations

--- model/user_to_event_model.go
+++ model/user_to_event_model.go
@@ -3,10 +3,11 @@ package model
 import "github.com/google/uuid"
 
 type UserToEventModel struct {
-	Id        uuid.UUID `gorm:"type:uuid;primary_key"`
-	EventId   uuid.UUID `gorm:"type:uuid;column:event_id"`
-	UserId    uuid.UUID `gorm:"type:uuid;column:user_id"`
-	ProjectId uuid.UUID `gorm:"type:uuid;column:project_id"`
+	Id         uuid.UUID `gorm:"type:uuid;primary_key"`
+	EventId    uuid.UUID `gorm:"type:uuid;column:event_id"`
+	UserId     uuid.UUID `gorm:"type:uuid;column:user_id"`
+	ProjectId  uuid.UUID `gorm:"type:uuid;column:project_id"`
+	IsRedeemed bool      `gorm:"column:is_redeemed"`
 }
 
 func (UserToEventModel) TableName() string {
